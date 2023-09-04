671637ce7fd10cb0777495a96345223623a4a7f8
Onaolapo Taiwo <onaolapo459@gmail.com>
2023-09-04 18:28:39 +0100

    updates

--- InviteMe/Program.cs
+++ InviteMe/Program.cs
@@ -59,10 +59,11 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     var context = services.GetRequiredService<ApplicationDbContext>();
+
     if (context.Database.GetPendingMigrations().Any())
     {
         context.Database.Migrate();
-    }
+    }    
 }
 
 app.Run();
